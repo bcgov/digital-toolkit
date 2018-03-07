965eb43c80b4cbfa911107db7df473862d2b8f6c
Jonathan Bell <jonathanbell.ca@gmail.com>
2018-03-07 13:40:07 -0800

    Reorganize static assets

--- src/app.js
+++
@@ -1,7 +0,0 @@
-import './js/bootstrap/bootstrap.js';
-import './js/bootstrap/ie10-viewport-bug-workaround.js';
-import './js/bc-gov/navMenu.js';
-import './js/bc-gov/misc.js';
-import './js/innovators-toolkit.js';
-
-import './scss/base.scss';
